35f5c5c8d989ede379cec8c3d225e717adc96f36
luiseduardo85 <77890900+luiseduardo85@users.noreply.github.com>
2023-11-30 12:52:10 -0300

    Dia 17 - OOP com GUI
    Aprendendo conceitos de OOP com interface, utilizando o modulo Turtle.

--- main.py
+++ main.py
@@ -1,49 +1,27 @@
-import random
-from game_data import data
-from art import *
-from replit import clear
+from turtle import Turtle, Screen
 
-print(logo)
+tiny = Turtle()
+tiny.shape("turtle")
+tiny.color("SteelBlue3")
+
+stop = True
+
+def draw(num_sides):
+    angle = 360 / num_sides
+    for i in range(num_sides):
+        tiny.forward(100)
+        tiny.right(angle)
 
 
-def get_random():
-    return random.choice(data)
+while stop:
+    dont_stop = input("deseja criar uma figura? ").lower()
+    if dont_stop == "yes":
+        num_sides = int(input("Digite a quantidade de lados: "))
+        draw(num_sides)
+    elif dont_stop == "no":
+        stop = False
 
 
-aux = False
-score = 0
-a = get_random()
-b = get_random()
 
-while aux != True:
-
-    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
-
-    print(vs)
-
-    print(f"Compare B: {b['name']}, a {b['description']}, from {b['country']}")
-    user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
-    while a == b:
-        b = random.choice(data)
-    if user_choice == 'a':
-        if a['follower_count'] > b['follower_count']:
-            a = b
-            b = random.choice(data)
-            score += 1
-            clear()
-            print(logo)
-            print(f"You're right! current score: {score}")
-        else:
-            print(f"This is your final score: {score}")
-            aux = True
-    elif user_choice == 'b':
-        if b['follower_count'] > a['follower_count']:
-            a = b
-            b = random.choice(data)
-            score += 1
-            clear()
-            print(logo)
-            print(f"You're right! current score: {score}")
-        else:
-            print(f"Sorry, that's wrong. Final score: {score}")
-            aux = True
+my_screen = Screen()
+my_screen.exitonclick()
