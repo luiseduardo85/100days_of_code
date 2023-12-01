3d72a04be11f039142206da496e1e39a94968e94
luiseduardo85 <77890900+luiseduardo85@users.noreply.github.com>
2023-12-01 11:47:34 -0300

    Dia 18 - Hirst-Painting
    Neste dia foram aplicados conceitos de OOP para a criação de um pintura de Hirst. Também foi utilizado o biblioteca colorgram pra extrair as cores de uma das pinturas de Hirst.

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+import random
+import turtle
 from turtle import Turtle, Screen
+import random
+
+rgb_colors = [(229, 228, 227), (226, 224, 225), (198, 175, 119), (125, 36, 23), (187, 157, 50), (170, 104, 56), (5, 56, 83), (201, 216, 205), (109, 67, 85), (39, 35, 34), (223, 224, 227), (84, 141, 61), (20, 122, 175), (111, 161, 176), (75, 38, 48), (8, 67, 47), (65, 154, 134), (132, 41, 43), (184, 98, 81), (183, 180, 181), (210, 200, 108), (178, 201, 186), (150, 180, 170), (90, 143, 158), (28, 81, 59), (193, 190, 192), (17, 78, 98), (215, 184, 172), (190, 190, 195), (78, 72, 31)]
+turtle.colormode(255)
 
 tiny = Turtle()
+
+tiny.penup()
+tiny.hideturtle()
 tiny.shape("turtle")
-tiny.color("SteelBlue3")
+tiny.color("SkyBlue")
 
-stop = True
+tiny.setheading(225)
+tiny.forward(300)
+tiny.setheading(0)
+dots = 100
 
-def draw(num_sides):
-    angle = 360 / num_sides
-    for i in range(num_sides):
-        tiny.forward(100)
-        tiny.right(angle)
+for dots in range(1, dots + 1):
+    tiny.dot(20, random.choice(rgb_colors))
+    tiny.forward(50)
 
-
-while stop:
-    dont_stop = input("deseja criar uma figura? ").lower()
-    if dont_stop == "yes":
-        num_sides = int(input("Digite a quantidade de lados: "))
-        draw(num_sides)
-    elif dont_stop == "no":
-        stop = False
-
+    if dots % 10 == 0:
+        tiny.setheading(90)
+        tiny.forward(50)
+        tiny.setheading(180)
+        tiny.forward(500)
+        tiny.setheading(0)
 
 
-my_screen = Screen()
-my_screen.exitonclick()
+screen = Screen()
+screen.exitonclick()
